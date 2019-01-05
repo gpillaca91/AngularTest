a4dff0fcd57eee61c0ca87c790f655aa3b621e9c
Gianmarco Pillaca <gpillaca91@gmail.com>
2019-01-04 22:13:48 -0500

    sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { PortafolioComponent } from './components/pages/portafolio/portafolio.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule
